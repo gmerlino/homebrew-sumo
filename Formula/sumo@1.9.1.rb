eab2e5786a94f407a893166eef9feee411378920
Giovanni Merlino <gmerlino@unime.it>
2026-01-24 15:14:25 +0100

    Comment out cxxstdlib_check in sumo formula
    Comment out cxxstdlib_check to address dependency issues.

--- Formula/sumo@1.9.1.rb
+++ Formula/sumo@1.9.1.rb
@@ -32,7 +32,7 @@ class SumoAT191 < Formula
   # workaround due to dependency gdal -> numpy -> openblas -> gcc (originally gfortran)
   # (use 'brew deps --tree sumo' to see dependencies of higher levels)
   # also see: https://github.com/davidchall/homebrew-hep/issues/28
-  cxxstdlib_check :skip
+  #cxxstdlib_check :skip
 
   def install
     cmake_args = *std_cmake_args
